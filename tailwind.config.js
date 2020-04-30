46080b118b2072b2a93a657111850978f49f5fa2
royappeldoorn <roy.appeldoorn@gmail.com>
2020-04-30 19:30:48 +0200

    Added tailwind gradient border plugin and customized colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,45 @@
  ** Docs: https://tailwindcss.com/docs/configuration
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
+
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        body: ['Inter', 'sans-serif'],
+        display: ['Inter', 'sans-serif']
+      },
+      lineHeight: {
+        'extra-loose': '2.5'
+      },
+      minHeight: {
+        '(screen-16)': 'calc(100vh - 5.25rem)'
+      },
       colors: {
-        primary: 'hsl(253, 35%, 10%)',
-        secondary: 'hsl(228, 32%, 15%)'
+        tracked: {
+          dark: '#6B01FF',
+          normal: '#A901FF',
+          light: '#EA01FF'
+        },
+        purple: {
+          '100': '#EECCFF',
+          '200': '#DC99FF',
+          '300': '#CB66FF',
+          '400': '#BA33FF',
+          '500': '#A901FF',
+          '600': '#8700CC',
+          '700': '#650099',
+          '800': '#430066',
+          '900': '#220033'
+        },
+        gray: {
+          ...colors.gray,
+          '700': '#282828',
+          '800': '#181818',
+          '900': '#121212'
+        }
       },
       spacing: {
         '1/2': '50%',
@@ -38,9 +71,39 @@ module.exports = {
         '9/12': '75%',
         '10/12': '83.333333%',
         '11/12': '91.666667%'
-      }
+      },
+      linearBorderGradients: {
+        directions: {
+          // defaults to these values
+          t: 'to top',
+          tr: 'to top right',
+          r: 'to right',
+          br: 'to bottom right',
+          b: 'to bottom',
+          bl: 'to bottom left',
+          l: 'to left',
+          tl: 'to top left'
+        },
+        colors: {
+          'pink-purple': ['#EA00FF', '#6A00FF']
+        }
+      },
+      repeatingLinearBorderGradients: theme => ({
+        directions: theme('linearBorderGradients.directions'), // defaults to the same values as linearBorderGradients’ directions
+        colors: theme('linearBorderGradients.colors'), // defaults to {}
+        lengths: {
+          // defaults to {}
+          sm: '25px',
+          md: '50px',
+          lg: '100px'
+        }
+      })
     }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    linearBorderGradients: ['responsive'], // defaults to ['responsive']
+    repeatingLinearBorderGradients: ['responsive'] // defaults to ['responsive']
+  },
+  plugins: [require('tailwindcss-border-gradients')()]
 };
