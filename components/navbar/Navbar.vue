d15df9450572d62e17b9e265eecea4eac47c6068
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-02 16:14:25 +0200

    Added link to add tracklist

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <div class="shade"></div>
+    <div class="shade" />
     <header
       class="navbar py-1 border-t-4 border-gradient-r-pink-purple shadow-sm fixed w-full"
     >
       <div class="container mx-auto sm:flex sm:justify-between sm:items-center">
         <div class="flex items-center justify-between px-4 py-3">
           <div>
-            <a href="/"
-              ><img class="h-10" src="~/assets/logo_gradient.svg" alt="Tracked"
-            /></a>
+            <a href="/">
+              <img
+                class="h-10"
+                src="~/assets/logo_gradient.svg"
+                alt="Tracked"
+              >
+            </a>
           </div>
 
           <div class="sm:hidden">
@@ -39,13 +43,29 @@
             <nuxt-link
               to="/explore"
               class="block sm:px-4 mt-2 sm:mt-0 font-medium text-sm hover:text-white"
-              >Explore</nuxt-link
             >
+              Explore
+            </nuxt-link>
             <nuxt-link
               to="/tracklist/12"
               class="block sm:px-4 mt-2 sm:mt-0 font-medium text-sm hover:text-white"
-              >Tracklist</nuxt-link
             >
+              Tracklist
+            </nuxt-link>
+            <nuxt-link
+              v-if="isOpen"
+              to="/explore"
+              class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+              </svg>
+              <span>Download</span>
+            </nuxt-link>
           </div>
 
           <div class="mx-4 border-t border-white opacity-25 sm:hidden" />
@@ -54,29 +74,44 @@
               <img
                 class="h-8 w-8 rounded-full border-2 border-gray-500"
                 src="https://randomuser.me/api/portraits/women/55.jpg"
-              />
+              >
               <span class="ml-3 font-semibold">Jane Doe</span>
             </div>
-
             <div class="mt-4">
-              <a href="#" class="block font-medium text-sm hover:text-white"
-                >Account settings</a
-              >
+              <a href="#" class="block font-medium text-sm hover:text-white">
+                Account settings
+              </a>
               <a
                 href="#"
                 class="mt-2 block font-medium text-sm hover:text-white"
-                >Support</a
               >
+                Support
+              </a>
               <a
                 href="#"
                 class="mt-2 block font-medium text-sm hover:text-white"
-                >Sign out</a
               >
+                Sign out
+              </a>
             </div>
           </div>
         </nav>
         <nav :class="isOpen ? 'block' : 'hidden'" class="sm:block">
           <div class="sm:px-2 px-4 pt-2 pb-5 sm:py-0 sm:flex items-center">
+            <nuxt-link
+              v-if="isOpen"
+              to="/explore"
+              class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center"
+            >
+              <svg
+                class="w-4 h-4 mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+              </svg>
+              <span>Download</span>
+            </nuxt-link>
             <AccountDropdown class="hidden sm:block sm:ml-6" />
           </div>
         </nav>
@@ -86,33 +121,31 @@
 </template>
 
 <script>
-import AccountDropdown from '@/components/account/AccountDropdown.vue';
+import AccountDropdown from '@/components/account/AccountDropdown.vue'
 
 export default {
-  components: {
-    AccountDropdown
-  },
-  data() {
+  components: { AccountDropdown },
+  data () {
     return {
       isOpen: false
-    };
+    }
   },
-  mounted() {
-    document.addEventListener('click', this.documentClick);
+  mounted () {
+    document.addEventListener('click', this.documentClick)
   },
-  destroyed() {
-    document.removeEventListener('click', this.documentClick);
+  destroyed () {
+    document.removeEventListener('click', this.documentClick)
   },
   methods: {
-    documentClick(e) {
-      const el = this.$refs.menu;
-      const target = e.target;
+    documentClick (e) {
+      const el = this.$refs.menu
+      const target = e.target
       if (el !== target && !el.contains(target)) {
-        this.isOpen = false;
+        this.isOpen = false
       }
     }
   }
-};
+}
 </script>
 
 <style lang="postcss" scoped>
@@ -131,7 +164,7 @@ export default {
   position: fixed;
   height: 50px;
   width: 95%;
-  top: 20px;
+  top: 40px;
   border-radius: 10px;
   -webkit-filter: blur(50px);
   filter: blur(50px);
