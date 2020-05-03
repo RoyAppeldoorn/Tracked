86f8e292fd9db802c6f1caea6f832c5f045a5976
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-03 19:52:56 +0200

    Removed min screen height

--- pages/explore/index.vue
+++ pages/explore/index.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="min-h-(screen-16) overflow-x-hidden pt-24">
+  <div class="overflow-x-hidden pt-20">
     <div class="relative">
-      <div
-        class="circle circle-double w-24 h-24 sm:w-56 sm:h-56 rounded-full absolute"
-      ></div>
+      <div class="circle circle-double w-24 h-24 sm:w-56 sm:h-56 rounded-full absolute" />
     </div>
 
     <div class="container mx-auto relative">
@@ -13,32 +11,18 @@
       <section class="py-8 sm:py-64 flex justify-between">
         <div class="artist-display relative">
           <div class="flex mx-4">
-            <div
-              class="image-icon hidden sm:block h-24 w-24 rounded-full shadow-xl relative flex justify-center items-center sm:m-0 sm:mr-8"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                class="icon-heart w-24 h-24 fill-current text-white"
-              >
+            <div class="image-icon hidden sm:block h-24 w-24 rounded-full shadow-xl relative flex justify-center items-center sm:m-0 sm:mr-8">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon-heart w-24 h-24 fill-current text-white">
                 <circle cx="12" cy="12" r="10" class="text-tracked-main" />
-                <path
-                  class="text-white"
-                  d="M12.88 8.88a3 3 0 114.24 4.24l-4.41 4.42a1 1 0 01-1.42 0l-4.41-4.42a3 3 0 114.24-4.24l.88.88.88-.88z"
-                />
+                <path class="text-white" d="M12.88 8.88a3 3 0 114.24 4.24l-4.41 4.42a1 1 0 01-1.42 0l-4.41-4.42a3 3 0 114.24-4.24l.88.88.88-.88z" />
               </svg>
             </div>
             <div class="details mb-4 sm:mb-0">
-              <h1
-                class="text-4xl sm:text-5xl text-tracked-text font-semibold mb-4 tracking-wider"
-              >
+              <h1 class="text-4xl sm:text-5xl font-semibold mb-4 tracking-wider">
                 Explore Tracklists
               </h1>
-              <h3
-                class="max-w-lg text-lg text-tracked-text leading-relaxed font-light"
-              >
-                Lists tracklists with the top number of votes amongst all
-                uploaded tracklists on the platform
+              <h3 class="max-w-lg text-lg leading-relaxed font-light">
+                Lists tracklists with the top number of votes amongst all uploaded tracklists on the platform
               </h3>
             </div>
           </div>
@@ -78,11 +62,7 @@ export default {}
   transform: scale(2.3);
   left: 55%;
   top: 20px;
-  background: linear-gradient(
-    180deg,
-    rgba(156, 2, 150, 0.8),
-    rgba(235, 5, 128, 0.8)
-  );
+  background: linear-gradient(180deg, rgba(156, 2, 150, 0.8), rgba(235, 5, 128, 0.8));
 }
 
 .circle-double:after {
@@ -101,10 +81,6 @@ export default {}
 }
 
 .image-icon {
-  background: linear-gradient(
-    140deg,
-    rgba(235, 5, 128, 0.7),
-    rgba(156, 2, 150, 0.7)
-  );
+  background: linear-gradient(140deg, rgba(235, 5, 128, 0.7), rgba(156, 2, 150, 0.7));
 }
 </style>
