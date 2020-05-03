9bca4294549e6b8c8c382a6e4d4707e8c843b7c5
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-03 19:51:07 +0200

    Formatting

--- components/account/AccountDropdown.vue
+++ components/account/AccountDropdown.vue
@@ -1,34 +1,19 @@
 <template>
   <on-click-outside :do="handleClickOutside">
     <div class="relative" @keydown.esc="isOpen = false">
-      <button
-        class="relative z-20 block h-12 w-12 rounded-full overflow-hidden border-2 border-purple-400 focus:outline-none focus:border-purple-200 hover:border-purple-200"
-        @click="isOpen = !isOpen"
-      >
-        <img
-          class="h-full w-full object-cover"
-          src="https://randomuser.me/api/portraits/lego/8.jpg"
-        />
+      <button class="relative z-20 block h-12 w-12 rounded-full overflow-hidden border-2 border-tracked-purple focus:outline-none focus:border-tracked-pink hover:border-tracked-pink" @click="isOpen = !isOpen">
+        <img class="h-full w-full object-cover" src="https://randomuser.me/api/portraits/lego/8.jpg" />
       </button>
-      <div
-        v-if="isOpen"
-        class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl z-20"
-      >
-        <a
-          href="#"
-          class="block px-4 py-2 text-gray-600 font-medium text-sm hover:bg-purple-500 hover:text-white"
-          >Account settings</a
-        >
-        <a
-          href="#"
-          class="block px-4 py-2 text-gray-600 font-medium text-sm hover:bg-purple-500 hover:text-white"
-          >Support</a
-        >
-        <a
-          href="#"
-          class="block px-4 py-2 text-gray-600 font-medium text-sm hover:bg-purple-500 hover:text-white"
-          >Sign out</a
-        >
+      <div v-if="isOpen" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl z-20">
+        <a href="#" class="block px-4 py-2 text-gray-600 font-medium text-sm hover:bg-purple-500 hover:text-white">
+          Account settings
+        </a>
+        <a href="#" class="block px-4 py-2 text-gray-600 font-medium text-sm hover:bg-purple-500 hover:text-white">
+          Support
+        </a>
+        <a href="#" class="block px-4 py-2 text-gray-600 font-medium text-sm hover:bg-purple-500 hover:text-white">
+          Sign out
+        </a>
       </div>
     </div>
   </on-click-outside>
