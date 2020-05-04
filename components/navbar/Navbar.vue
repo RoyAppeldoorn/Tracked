54247457854f78528d448e31004b7bb6408ad9f3
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-04 17:57:02 +0200

    Removed unused class names

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div v-show="!isOpen" class="shade" />
+    <div v-if="!isOpen" class="shade" />
     <header class="navbar py-1 border-t-4 border-gradient-r-pink-purple shadow-sm fixed w-full">
       <div class="container mx-auto sm:flex sm:justify-between sm:items-center px-4 py-3">
         <div class="flex items-center">
           <div>
-            <a href="/">
+            <nuxt-link to="/">
               <img class="h-10 mr-10" src="~/assets/logo_gradient.svg" alt="Tracked" />
-            </a>
+            </nuxt-link>
           </div>
 
           <div class="lg:block hidden px-4">
@@ -17,13 +17,13 @@
                   <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
                 </svg>
               </button>
-              <input class="border-2 border-gradient-tr-pink-purple bg-transparent pl-10 pr-2 py-2 text-sm focus:bg-gray-100 text-gray-800" placeholder="Search by keywords" />
+              <input class="border-2 border-gradient-tr-pink-purple bg-transparent pl-10 pr-2 py-2 text-sm placeholder-gray-400 focus:placeholder-gray-600 outline-none" placeholder="Search by keywords" />
             </div>
           </div>
 
           <nuxt-link to="/explore" class="lg:hidden flex font-medium text-sm hover:text-white group">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-current text-gray-100 h-6 w-6">
-              <path class="secondary" d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 01-2.12 2.12l-3-3a1.5 1.5 0 01-.44-1.14l-1.1-1.1a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" />
+              <path d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 01-2.12 2.12l-3-3a1.5 1.5 0 01-.44-1.14l-1.1-1.1a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" />
             </svg>
           </nuxt-link>
 
@@ -41,20 +41,20 @@
           <div class="sm:p-0 sm:px-4 py-5 sm:flex items-center">
             <nuxt-link to="/explore" class="flex sm:mx-4 mt-2 sm:mt-0 font-medium text-sm hover:text-white group">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-current text-gray-100 h-6 w-6">
-                <path class="secondary" d="M14 10h2a1 1 0 01.81 1.58l-5 7A1 1 0 0110 18v-4H8a1 1 0 01-.81-1.58l5-7A1 1 0 0114 6v4z" />
+                <path d="M14 10h2a1 1 0 01.81 1.58l-5 7A1 1 0 0110 18v-4H8a1 1 0 01-.81-1.58l5-7A1 1 0 0114 6v4z" />
               </svg>
               <span class="ml-2 block group-hover:text-white">Explore</span>
             </nuxt-link>
             <nuxt-link to="/tracklist/12" class="flex sm:mx-4 mt-2 sm:mt-0 font-medium text-sm hover:text-white group">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-current text-gray-100 h-6 w-6">
-                <path class="primary" d="M22 17a1 1 0 01-2 0v-5a8 8 0 10-16 0v5a1 1 0 01-2 0v-5a10 10 0 1120 0v5z" />
-                <path class="secondary" d="M7 12a2 2 0 012 2v6a2 2 0 11-4 0v-6c0-1.1.9-2 2-2zm10 0a2 2 0 012 2v6a2 2 0 11-4 0v-6c0-1.1.9-2 2-2z" />
+                <path d="M22 17a1 1 0 01-2 0v-5a8 8 0 10-16 0v5a1 1 0 01-2 0v-5a10 10 0 1120 0v5z" />
+                <path d="M7 12a2 2 0 012 2v6a2 2 0 11-4 0v-6c0-1.1.9-2 2-2zm10 0a2 2 0 012 2v6a2 2 0 11-4 0v-6c0-1.1.9-2 2-2z" />
               </svg>
               <span class="ml-2 block group-hover:text-white">Tracklists</span>
             </nuxt-link>
             <nuxt-link to="/tracklist/add" class="flex sm:mx-4 mt-2 sm:mt-0 font-medium text-sm hover:text-white group bg-transparent hover:bg-gradient-bl-pink-purple font-semibold py-2 px-4 border border-gradient-bl-pink-purple rounded">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-current text-gray-100 h-6 w-6">
-                <path class="secondary" d="M13 11h4a1 1 0 010 2h-4v4a1 1 0 01-2 0v-4H7a1 1 0 010-2h4V7a1 1 0 012 0v4z" />
+                <path d="M13 11h4a1 1 0 010 2h-4v4a1 1 0 01-2 0v-4H7a1 1 0 010-2h4V7a1 1 0 012 0v4z" />
               </svg>
               <span class="ml-2 block group-hover:text-white">Upload</span>
             </nuxt-link>
@@ -123,7 +123,10 @@ export default {
   background-color: rgba(235, 5, 128, 0.2);
   position: fixed;
   height: 50px;
+  margin: 0 auto;
   width: 95%;
+  left: 0;
+  right: 0;
   top: 40px;
   border-radius: 10px;
   -webkit-filter: blur(50px);
