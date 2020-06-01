c461121eeca3604ea824d10541c8b8858775eae1
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-01 20:36:26 +0200

    check for each request if an idToken is set

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,6 @@
 export default function({ $axios, store }) {
   $axios.onRequest(config => {
-    if (store.state['user/getToken']) {
+    if (store.getters['user/getToken']) {
       config.headers.common['X-Firebase-Auth'] = store.getters['user/getToken']
     }
   })
