3446f46c9e8b9deff57f0d1e17c0f55aa33107ac
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-04 16:28:19 +0200

    Added automatic idtoken refresh on 401 from the server

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,53 @@
-export default function({ $axios, store }) {
-  $axios.onRequest(config => {
-    if (store.getters['user/getToken']) {
-      config.headers.common['X-Firebase-Auth'] = store.getters['user/getToken']
+export default function({ $axios, $fireAuth, store }) {
+  let isRefreshing = false
+  let subscribers = []
+
+  $axios.onRequest(
+    config => {
+      if (store.getters['user/token']) {
+        config.headers.common['X-Firebase-Auth'] = store.getters['user/token']
+      }
+    },
+    response => {
+      return response
+    },
+    error => {
+      const {
+        config,
+        response: { status }
+      } = error
+      const originalRequest = config
+
+      if (status === 401) {
+        if (!isRefreshing) {
+          isRefreshing = true
+          $fireAuth.currentUser.getIdToken(true).then(newIdToken => {
+            isRefreshing = false
+            store.commit('SET_AUTH_TOKEN', newIdToken)
+            onRefreshed(newIdToken)
+          })
+        }
+
+        const retryOriginalRequest = new Promise(resolve => {
+          subscribeTokenRefresh(newToken => {
+            // replace the expired token and retry
+            originalRequest.headers['X-Firebase-Auth'] = newToken
+            resolve($axios(originalRequest))
+          })
+        })
+
+        return retryOriginalRequest
+      }
+
+      return Promise.reject(error)
     }
-  })
+  )
+
+  function subscribeTokenRefresh(cb) {
+    subscribers.push(cb)
+  }
+
+  function onRefreshed(newToken) {
+    subscribers = subscribers.filter(callback => callback(newToken))
+  }
 }
